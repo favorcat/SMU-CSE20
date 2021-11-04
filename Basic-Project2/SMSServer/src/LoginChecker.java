e4934fd35498ccf1e28b3093117cf94b99a05ad5
favorcat <favorcat.me@gmail.com>
2021-11-04 15:51:41 +0900

    211104 기초프로젝트2 학생관리프로그램 Server 에러 수정

--- Basic-Project2/SMSServer/src/LoginChecker.java
+++ Basic-Project2/SMSServer/src/LoginChecker.java
@@ -4,24 +4,24 @@ import java.util.StringTokenizer;
 
 public class LoginChecker {
     ArrayList<User> userInfor = new ArrayList<User>();
-    File dataFile = new File("../users.txt");
+    File dataFile = new File("/Users/favorcat/Github/SMU-CSE20/Basic-Project2/SMSServer/users.txt");
     String readData = null;
     StringTokenizer st;
 
     LoginChecker(){
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(dataFile));
-            // 한 줄씩 읽는다
-            while((readData=br.readLine()) != null){
-                st = new StringTokenizer(readData, "//");
-                String userID = st.nextToken();
-                String userPassword = st.nextToken();
-                User user = new User(userID, userPassword);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(dataFile)); // 파일 리더 선언
+            while ((readData = br.readLine()) != null) {
+                // 끝까지 읽는다.
+                st = new StringTokenizer(readData, "//"); // 구분자는
+                String userId= st.nextToken(); // 첫번째 받아주기
+                String userPassword = st.nextToken(); //  구분자 후 두번째
+                User user = new User(userId, userPassword);
                 userInfor.add(user);
             }
             br.close();
-        } catch(Exception e){
-            System.out.println("Server> LoginChecker 에러 발생");
+        } catch (IOException error) {
+            System.out.println(error.getMessage());
         }
     }
 
--- Basic-Project2/SMSServer/src/Server.java
+++ Basic-Project2/SMSServer/src/Server.java
@@ -2,32 +2,33 @@ import java.io.*;
 import java.io.IOException;
 import java.net.*;
 import java.net.SocketException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Server {
+
+public class Server {   // 메인클래스
     ServerSocket ss = null;
     ArrayList<ConnectedClient> clients = new ArrayList<ConnectedClient>();
-    LoginChecker lc = null;
-
+    LoginChecker lc;
     public static void main(String[] args) {
         Server server = new Server();
         server.lc = new LoginChecker();
-
         try {
-            server.ss = new ServerSocket(555556);
-            System.out.println("Server> Server Socket is Created");
-            while(true){
+            server.ss = new ServerSocket(20000);
+            System.out.println("Server > Server Socket is Created...");
+            while(true) {
                 Socket socket = server.ss.accept();
-                // 소켓이 없어지기 전에 소켓을 상속시키는 것
-                ConnectedClient c = new ConnectedClient(socket, server);
+                ConnectedClient  c = new ConnectedClient(socket, server);
                 server.clients.add(c);
-
                 c.start();
             }
-        } catch(Exception e) {
-            System.out.println("Sever> 예외 발생");
+
+        }catch(SocketException e) {
+            System.out.println("Server > 소켓 관련 예외 발생, 서버종료");
+        }catch(IOException e) {
+            System.out.println("Server > 입출력 예외 발생");
         }
+
     }
+
 }
