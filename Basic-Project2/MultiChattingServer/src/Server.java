7723e9b8fb1f7b58459dbba9aac33e1dc9f018cd
favorcat <favorcat.me@gmail.com>
2021-11-17 19:44:21 +0900

    중복 파일

--- Basic-Project2/MultiChattingServer/src/Server.java
+++ Basic-Project2/MultiChattingServer/src/Server.java
@@ -11,31 +11,36 @@ public class Server {
     public static void main(String[] args) {
         Server server = new Server();
         try {
-            server.ss = new ServerSocket(12345);
+            server.ss = new ServerSocket(60000);
             System.out.println("Server> Server Socket is Created...");
             while(true) {
                 Socket socket = server.ss.accept();
-                ConnectedClient c = new ConnectedClient(socket);
+                ConnectedClient c = new ConnectedClient(socket, server);
                 server.clients.add(c);
-                c.start();  // 쓰레드 시작
+                c.start();
             }
+
         } catch(SocketException e) {
-            System.out.println("Server> 소켓 관련 예외 발생, 서버종료");
+            System.out.println("Server> 서버종료");
         } catch(IOException e) {
             System.out.println("Server> 입출력 예외 발생");
         }
     }
 }
 
-class ConnectedClient extends Thread {
+class ConnectedClient extends Thread{
     Socket socket;
     OutputStream outStream;
     DataOutputStream dataOutStream;
     InputStream inStream;
     DataInputStream dataInStream;
+    String msg;
+    String uName;
+    Server server;
 
-    ConnectedClient(Socket _s){
+    ConnectedClient(Socket _s, Server _ss){
         this.socket = _s;
+        this.server = _ss;
     }
 
     @Override
@@ -46,10 +51,24 @@ class ConnectedClient extends Thread {
             dataOutStream = new DataOutputStream(outStream);
             inStream = this.socket.getInputStream();
             dataInStream = new DataInputStream(inStream);
+
             dataOutStream.writeUTF("Welcome to this server!!!");
+
+            uName = dataInStream.readUTF();
+            dataOutStream.writeUTF(uName + "님이 입장하였습니다.");
+
             while(true) {
-                String msg = dataInStream.readUTF();
-                System.out.println("Server> " + this.socket.toString() + ": " + msg);
+                msg = dataInStream.readUTF();
+                System.out.println("Server> " + this.uName + ": " + msg);
+
+                for(int i=0; i<server.clients.size(); i++){
+                    if( !(uName.equals(server.clients.get(i).uName)) ) {
+                        outStream = server.clients.get(i).socket.getOutputStream();
+                        dataOutStream = new DataOutputStream(outStream);
+                        dataOutStream.writeUTF("["+ uName + "] : " + msg);
+                    }
+                }
+
             }
 
         } catch(IOException e) {
